33d430092006382984f61b43da216b42be4da225
Abhi <abhishekvns014@gmail.com>
2024-08-14 18:34:26 +0530

    Add Favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -6,7 +6,7 @@ export default function Head() {
       </title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Unleashing the Power of Your Financial Future - GetPaisa.com" />
-      <link rel="icon" href="/images1/favicon.ico" />
+      <link rel="icon" href="src/app/favicon.ico" />
     </>
   );
 }
