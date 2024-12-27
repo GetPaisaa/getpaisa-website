f2090f848be1d9d8cd0d508f3535b1600db448a3
Abhi <abhishek.singh@propkar.com>
2024-12-27 14:55:51 +0530

    changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
                 />
               </Link>
               <p className="mb-8 font-semibold max-w-[270px] text-base text-gray-7">
-                  Propkar Securities Pvt Ltd™
+                  PROPKAR SECURITIES PVT LTD™
               </p>
               <div className="-mx-3 flex items-center">
               
