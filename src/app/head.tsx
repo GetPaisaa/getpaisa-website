77157a23c32a5b2d1198be4a6310e7835720aa1b
Abhi <abhishek.singh@propkar.com>
2025-01-06 17:12:15 +0530

    changes

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,7 +5,7 @@ export default function Head() {
          GetPaisa - Unleashing the Power of Your Financial Future
       </title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="GetPaisa.com - Unleashing the Power of Your Financial Future" />
+      <meta name="description" content="GetPaisa - Unleashing the Power of Your Financial Future" />
       <link rel="icon" href="app/favicon.ico" />
     </>
   );
