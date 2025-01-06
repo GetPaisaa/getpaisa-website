3faa728031bfc792ef23a846c63580851dfde913
Abhi <abhishek.singh@propkar.com>
2025-01-06 17:22:53 +0530

    changes

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -6,7 +6,7 @@ export default function Head() {
       </title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="GetPaisa - Unleashing the Power of Your Financial Future" />
-      <link rel="icon" href="app/favicon.ico" />
+      <link rel="icon" href="https://getpaisa-website.s3.ap-south-1.amazonaws.com/favicon.ico" />
     </>
   );
 }
