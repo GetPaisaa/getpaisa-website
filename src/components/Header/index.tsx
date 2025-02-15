f6ec43a0a0e125e3793d4432fe8d9334cf1f5785
abhishekxsingh <abhishek.singh@propkar.com>
2025-02-15 17:57:47 +0530

    refactor: clean up imports in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,9 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-
 import menuData from "./menuData";
-
 import DownloadNow from "@/components/Common/DownloadNow";
 
 const Header = () => {
