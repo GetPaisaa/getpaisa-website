635e44db68863ae9cf40d86614855d3d62c83bfe
PROPKAR-STPL <abhishek.singh@propkar.com>
2025-08-18 16:30:47 +0530

    Update head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,6 +5,7 @@ const Head = () => {
          GetPaisa - Unleashing the Power of Your Financial Future
       </title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="google-site-verification" content="vjY_jB1LDz4LgSeLryBuVElY0ua-MewjiyiVTvUpm5k" />
       <meta name="description" content="GetPaisa - Unleashing the Power of Your Financial Future" />
       <link rel="icon" href="https://getpaisa-website.s3.ap-south-1.amazonaws.com/favicon.ico" />
     </>
